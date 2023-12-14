2d556eb012c2a9965d860b0d853dcdba153ed8d1
Renan Machado <renan.machado@coinbase.com>
2023-12-14 12:08:05 -0300

    estudo 5 busca cep

--- 4-pacotes-importantes/5-Busca-CEP/main.go
+++ 4-pacotes-importantes/5-Busca-CEP/main.go
@@ -8,7 +8,7 @@ import (
 	"os"
 )
 
-type ViaCEP struct {
+type ViaCEPResponse struct {
 	Cep         string `json:"cep"`
 	Logradouro  string `json:"logradouro"`
 	Complemento string `json:"complemento"`
@@ -23,27 +23,34 @@ type ViaCEP struct {
 
 func main() {
 	for _, cep := range os.Args[1:] {
-		req, err := http.Get("http://viacep.com.br/ws/" + cep + "/json/")
+		req, err := http.Get(fmt.Sprintf("https://viacep.com.br/ws/%s/json/", cep))
 		if err != nil {
-			fmt.Fprintf(os.Stderr, "Erro ao fazer requisição: %v\n", err)
+			fmt.Fprintf(os.Stderr, "Erro ao fazer a requisição: %v\n", err)
 		}
 		defer req.Body.Close()
 		res, err := io.ReadAll(req.Body)
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "Erro ao ler resposta: %v\n", err)
 		}
-		var data ViaCEP
+		var data ViaCEPResponse
 		err = json.Unmarshal(res, &data)
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "Erro ao fazer parse da resposta: %v\n", err)
 		}
-		file, err := os.Create("cidade.txt")
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "Erro ao criar arquivo: %v\n", err)
+
+		fmt.Println(data)
+
+		// Create file if not exists
+		var file *os.File
+		if file, err = os.OpenFile("./cidade.txt", os.O_APPEND|os.O_WRONLY, 0644); err != nil {
+			if file, err = os.Create("./cidade.txt"); err != nil {
+				fmt.Fprintf(os.Stderr, "Erro ao criar arquivo: %v\n", err)
+			}
 		}
 		defer file.Close()
-		_, err = file.WriteString(fmt.Sprintf("CEP: %s, Localidade: %s, UF: %s", data.Cep, data.Localidade, data.Uf))
-		fmt.Println("Arquivo criado com sucesso!")
-		fmt.Println("Cidade: ", data.Localidade)
+
+		if _, err = file.WriteString(fmt.Sprintf("%+v\n", data)); err != nil {
+			fmt.Fprintf(os.Stderr, "Erro ao escrever no arquivo: %v\n", err)
+		}
 	}
 }
