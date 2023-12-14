e52488dbf638c5ebfaba4bf1d26086105de13901
Renan Machado <renan.machado@coinbase.com>
2023-12-14 17:04:11 -0300

    estudo 5 busca cep http server

--- 4-pacotes-importantes/5-BuscaCEP-http/main.go
+++ 4-pacotes-importantes/5-BuscaCEP-http/main.go
@@ -2,20 +2,21 @@ package main
 
 import (
 	"encoding/json"
-	"io/ioutil"
+	"io"
 	"net/http"
 )
 
-type ViaCEP struct {
+type ViaCEPResponse struct {
 	Cep         string `json:"cep"`
 	Logradouro  string `json:"logradouro"`
 	Complemento string `json:"complemento"`
 	Bairro      string `json:"bairro"`
 	Localidade  string `json:"localidade"`
 	Uf          string `json:"uf"`
-	Unidade     string `json:"unidade"`
 	Ibge        string `json:"ibge"`
 	Gia         string `json:"gia"`
+	Ddd         string `json:"ddd"`
+	Siafi       string `json:"siafi"`
 }
 
 func main() {
@@ -28,41 +29,48 @@ func BuscaCepHandler(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusNotFound)
 		return
 	}
+
 	cepParam := r.URL.Query().Get("cep")
 	if cepParam == "" {
 		w.WriteHeader(http.StatusBadRequest)
 		return
 	}
-	cep, error := BuscaCep(cepParam)
-	if error != nil {
+
+	cep, err := BuscaCep(cepParam)
+	if err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
 		return
 	}
+
+	result, err := json.Marshal(cep)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
-	// result, err := json.Marshal(cep)
-	// if err != nil {
-	// 	w.WriteHeader(http.StatusInternalServerError)
-	// 	return
-	// }
-	// w.Write(result)
+	w.Write(result)
 	json.NewEncoder(w).Encode(cep)
 }
 
-func BuscaCep(cep string) (*ViaCEP, error) {
-	resp, error := http.Get("https://viacep.com.br/ws/" + cep + "/json/")
-	if error != nil {
-		return nil, error
+func BuscaCep(cep string) (*ViaCEPResponse, error) {
+	req, err := http.Get("https://viacep.com.br/ws/" + cep + "/json/")
+	if err != nil {
+		return nil, err
 	}
-	defer resp.Body.Close()
-	body, error := ioutil.ReadAll(resp.Body)
-	if error != nil {
-		return nil, error
+	defer req.Body.Close()
+
+	res, err := io.ReadAll(req.Body)
+	if err != nil {
+		return nil, err
 	}
-	var c ViaCEP
-	error = json.Unmarshal(body, &c)
-	if error != nil {
-		return nil, error
+
+	var data ViaCEPResponse
+	err = json.Unmarshal(res, &data)
+	if err != nil {
+		return nil, err
 	}
-	return &c, nil
+
+	return &data, nil
 }
